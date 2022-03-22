7f726d8b4a9b5d83dd94fd6e0516a7c2378b4433
robo <robolab1902@naver.com>
2022-03-22 22:38:50 +0900

    프로젝트 변경

---
+++ source/MyMuse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyMuse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
